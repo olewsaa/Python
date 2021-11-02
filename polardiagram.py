3abf90dc863396cfae24794cdfc71df6b84fa36b
Ole W. Saastad <olewsaa@online.no>
2021-11-02 20:04:34 +0100

    Polardiagram with data from OpenCPN
    Changed windspeed to m/s.

--- polardiagram.py
+++ polardiagram.py
@@ -2,12 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 file = open("polar.csv")
-
 pol = np.loadtxt(file, delimiter=";")
 speed=np.zeros([38,11])
 
-windspeed=pol[0][2:-1]/1.852 
-
+#windspeed=pol[0][2:-1]
+#windspeed=[int(x/1.852) for x in windspeed]
+windspeed=[int(x/1.852) for x in pol[0][2:-1]]
+#print(windspeed)
 theta=pol[:,0]
 theta=[x/180.0*3.1415 for x in theta]
 
@@ -22,12 +23,15 @@ ax.set_thetamax(180)
 ax.set_theta_direction(1)
 ax.set_theta_offset(-90/180*np.pi)
 
-for p in range(len(speed[0,:])):
+#print(speed[:,9])
+for p in range(1,len(speed[0,1:]-1)):
+    #print(p,speed[:,p])
     c = ax.plot(theta, speed[:,p],linewidth=3)
     c = ax.scatter(theta, speed[:,p], marker="o", s=100)
     
-lg=[' ']*len(windspeed)
+lg=[' ']*(len(windspeed))
 for p in range(len(windspeed)):
     lg[p]=str(int(windspeed[p]))
+
 ax.legend(lg)
 fig.savefig("polar.png")
